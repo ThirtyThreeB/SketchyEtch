9dc4f56f0000e0cd5e199daa7ed75559fa458001
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-10-09 15:25:41 -0400

    function to make number of boxes assigned by user input field works correctly and displays those boxes correctly

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+$(document).ready(function(){
+  $( "button" ).on( "click", addDivs );
+  
+});
 
 // need a var for squares per side
-let boxes = 4;
 
+
+//console.log(boxesPerSide);
 //number of boxes per row === to number of rows
 
 //while loop up to number of rows, looping to make boxes per row
 function addDivs(){
+  console.log("$boxes=", $("#boxesPerSide").val());
+
+let boxes = $("#boxesPerSide").val();
+console.log('boxes=', boxes);
+
+let sideLength = 500 / boxes;
+console.log("sideLength=", sideLength);
+
 let i=0;
 while (i<boxes) {
   for(let j=0; j<boxes; j++){
-    //makes some divs
-    let test = j;
-   $('ul').append($("<li></li>").text(j));
-    // $("<p></p>").text("Text.");   // Create with jQuery
-
-      // console.log('make some divs!');
-  }
+   $('ul').append($("<li></li>").text(j).css({"height": sideLength + "px", "width": sideLength + "px"}));
+   }
      $('ul').append($("<p></p>"));
-
-  //console.log('break');
   i++;
 }
 }
 
-$(document).ready(function(){
-  $( "button" ).on( "click", addDivs );
-});
