ca39f297f7657484a94b38e1bfb4ec3501705d9b
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-10-11 13:30:54 -0400

    still works, no real progress

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 $(document).ready(function(){
   $( "button" ).on( "click", addLis );//adds all the <li>s to the DOM
 
-  // $("li").mouseenter(function(){//this doesn't work!!!
-  //        $(this).fadeIn('slow', function() {
+  // $( "button .reset" ).on( "click", promptFn );//adds all the <li>s to the DOM
 
-  // });  
-  // $("li").on("mouseleave", function(){
-  //   $(this).fadeOut(3000);
-  // });
-});
+  // $("li").hover(function(){
+  //        $(this).addClass('hovered');
+  //        }, function(){
+  //   $(this).removeClass('hovered');
+  });
 
-// need a var for squares per side
-
-
-//console.log(boxesPerSide);
-//number of boxes per row === to number of rows
 
 //while loop up to number of rows, looping to make boxes per row
 function addLis(){
@@ -31,12 +25,28 @@ while (i<boxes) {
   for(let j=0; j<boxes; j++){
    $('ul').append($("<li></li>").css({"height": sideLength + "px", "width": sideLength + "px"}));
    }//add .text(j) after .append() to have numbers appear in the boxes
-     $('ul').append($("<p></p>"));
+    // $('ul').append($("<p></p>"));
   i++;
 }
 }
+
+// function promptFn(){
+//   let boxNumber = prompt("enter number of boxes per side", "16");
+//   addLis(boxNumber);
+// }
 
 //having trouble getting boxes to fadeout on mouseleave.
 //using :hover worked well but was just on/off
 //if I had a fade delay I could have a nice mouse tail effect
 //maybe will focus on some other stuff first and return
+
+//to do:
+//x//make button and grid
+//X//hover effect
+//try addClass() instead of :hover
+//make change to box permanent
+
+//prompt??
+//reset button
+//add random color
+//add 10% black
