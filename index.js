43959c0251a27fe745b06a5ad24587d209a9a318
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-10-12 15:10:26 -0400

    finally got the boxes to change and stay changed

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
-  $("#boxMaker").on( "click", addLis);//adds all the <li>s to the DOM
+  $("#boxMaker").on("click", addLis);//adds all the <li>s to the DOM
 
-  $("#reset").on( "click", promptFn);//adds all the <li>s to the DOM
+  $("#reset").on("click", promptFn);//adds all the <li>s to the DOM
+
+
+ // $(".box").click(function(event) {
+ //    changeBox();  
+ //  });
+  //not sure why this isn't working
 
   // $("li").hover(function(){
   //        $(this).addClass('hovered');
   //        }, function(){
   //   $(this).removeClass('hovered');
-  });
+  
 
 
 //while loop up to number of rows, looping to make boxes per row
 function addLis(input){
-  console.log("$boxes=", $("#boxesPerSide").val());
+  // console.log("$boxes=", $("#boxesPerSide").val());
 let boxes = input;
 // let boxes = $("#boxesPerSide").val();
 console.log('boxes=', boxes);
@@ -25,10 +31,14 @@ while (i<boxes) {
   for(let j=0; j<boxes; j++){
    $('ul').append($("<li></li>").css({"height": sideLength + "px", "width": sideLength + "px"}));
    }//add .text(j) after .append() to have numbers appear in the boxes
-    // $('ul').append($("<p></p>"));
   i++;
 }
+
+  $("li").on('mouseover', function() {//turns out his had to be inside this fn block
+    $(event.target).addClass("hovered");
+  });    
 }
+
 
 function promptFn(){
   reset();
@@ -38,11 +48,19 @@ function promptFn(){
   addLis(boxNumber);
 }
 
+function changeBox() {
+  $(event.target).css("background-color", "rgba(0, 170, 255, 0.9)");
+  console.log('changeBox works');
+}
 
 function reset() {
   $("li").remove();
   console.log('reset');
 }
+
+
+
+});
 //having trouble getting boxes to fadeout on mouseleave.
 //using :hover worked well but was just on/off
 //if I had a fade delay I could have a nice mouse tail effect
@@ -55,6 +73,6 @@ function reset() {
 //make change to box permanent
 
 //x//prompt
-//reset button
+//x//reset button
 //add random color
 //add 10% black
